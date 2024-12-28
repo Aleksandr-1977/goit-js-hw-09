81b23b71436fcd22ac84a97b449a3e1d380de3b9
Aleksandr-1977 <vasilenkobox@gmail.com>
2024-12-28 15:25:42 +0200

    add task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,44 @@
-console.log('Form');
+const input = document.querySelector('.feedback-form');
+let formData = {
+  email: '',
+  message: '',
+};
+const fillForm = () => {
+  try {
+    if (localStorage.length === 0) {
+      return;
+    }
+    const formDataFromLS = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+    formData = formDataFromLS;
+    for (const key in formDataFromLS) {
+      input.elements[key].value = formDataFromLS[key];
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+fillForm();
+const onFormField = event => {
+  const { target: formFieldEl } = event;
+  const fieldValue = formFieldEl.value;
+  const fieldName = formFieldEl.name;
+  formData[fieldName] = fieldValue;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+const onFeedbackSubmit = event => {
+  event.preventDefault();
+  const { email, message } = formData;
+
+  if (!email.trim() || !message.trim()) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  event.target.reset();
+  localStorage.removeItem('feedback-form-state');
+  //   formData = { email: '', message: '' };
+};
+input.addEventListener('input', onFormField);
+input.addEventListener('submit', onFeedbackSubmit);
